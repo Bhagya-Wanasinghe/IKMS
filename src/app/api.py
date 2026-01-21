5aec9d55068e4828826520c6e5f51075125aeb30
Bhagya-Wanasinghe <bhagyashamindi@gmail.com>
2026-01-22 03:24:55 +0530

    CORS

--- src/app/api.py
+++ src/app/api.py
@@ -21,7 +21,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://ikms-beta.vercel.app/"], 
+    allow_origins=["https://ikms-beta.vercel.app", "http://localhost:3000"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
